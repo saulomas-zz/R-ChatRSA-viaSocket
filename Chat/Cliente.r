4b8ad2395efb895739756cda02d70e1cce43c64d
Saulo <saulomas@gmail.com>
2019-03-17 20:57:02 -0300

    Client->Servidor = CHECK

--- Chat/Cliente.r
+++ Chat/Cliente.r
@@ -1,58 +1,76 @@
 require(primes)
 require(FRACTION)
+require(stringi)
 
 client <- function(){
+    #--------------------------------------------------------------------
     #Geração das Chaves Públicas e Privadas
+    maxPrimeNumber = 99
     print("Geracao da Chave Publica")
     isPrime = FALSE
     while(isPrime == FALSE) {
-        primNumber = sample(1:99,1)
+        primNumber = sample(1:maxPrimeNumber,1)
         if (primes::is_prime(primNumber)) {
             isPrime = TRUE
         }
     }
     
-    p = primNumber
+    cli_p = primNumber
 
     isPrime = FALSE
     while(isPrime == FALSE) {
-        primNumber = sample(1:99,1)
-        if (primNumber != p && primes::is_prime(primNumber)) {
+        primNumber = sample(1:maxPrimeNumber,1)
+        if (primNumber != cli_p && primes::is_prime(primNumber)) {
             isPrime = TRUE
         }
     }
 
-    q = primNumber
+    cli_q = primNumber
     
-    n_Cli = p*q
+    cli_n = cli_p*cli_q
 
-    fiN = (p-1)*(q-1)
+    cli_n_fi = (cli_p-1)*(cli_q-1)
     
-    e_Cli = 0
-    for (i in 2:fiN) {
-        if (gcd(fiN, i) == 1) {
-            e_Cli = i
+    cli_e = 0
+    for (i in 2:cli_n_fi) {
+        if (gcd(cli_n_fi, i) == 1) {
+            cli_e = i
             break
         }
     }
 
-    rm(primNumber)
-    rm(isPrime)
-    rm(i)
+    cli_d = 1
+    repeat {
+        if (cli_d < cli_n_fi && (cli_d*cli_e) %% cli_n_fi == 1) {
+            break
+        }
+        cli_d = cli_d + 1
+    }
     #--------------------------------------------------------------------
-
     print("Troca de chaves")
-    #kpuc = c(n_Cli,e_Cli)
-    kpuc = c(toString(n_Cli), toString(e_Cli))
+    #kpuc = c(cli_n,cli_e)
+    kpuc = paste(c(cli_n, cli_e), collapse = ",")
     con <- socketConnection(host="localhost", port=666, blocking=TRUE, server=FALSE, open="r+")
 
     # enviar a chave publica do cliente
-    write_resp = writeLines(kpuc, con, useBytes = TRUE)
+    write_resp = writeLines(kpuc, con)
+
     # receber a chave publica do servidor
-    kpus = readLines(con,2)
-    print(kpus)
+    kpus = readLines(con,1)
+    print(kpus)    
+
+    serv_n = as.integer(unlist(strsplit(kpus, split=",")))[1]
+    serv_e = as.integer(unlist(strsplit(kpus, split=",")))[2]
 
     close(con)
+    #--------------------------------------------------------------------
+    rm(maxPrimeNumber)
+    rm(primNumber)
+    rm(isPrime)
+    rm(i)
+    rm(kpuc)
+    rm(kpus)
+    #--------------------------------------------------------------------
 
     while(TRUE){
         con = socketConnection(host="localhost", port = 666, blocking=TRUE, server=FALSE, open="r+")
@@ -65,6 +83,21 @@ client <- function(){
         if(tolower(msg)=="q"){
             break
         }
+        
+        # cliente criptografa a mensagem e a envia para o servidor
+        # fazer aqui a criptografia
+        msgUTF8 = utf8ToInt(stringi::stri_enc_toascii(msg))
+        msgCrypt = (msgUTF8 ^ serv_e) %% serv_n
+        write_resp = writeLines(paste(as.character(msgCrypt), collapse = ","), con)
+
+        # cliente recebe mensagem enviada pelo servidor
+        data = readLines(con, 1)
+        
+        # cliente decriptografa a mensagem e a mostra na tela
+        # fazer aqui a decriptografia
+        msgDecrypt = (msgCrypt ^ cli_d) %% cli_n
+        msg = intToUtf8(msgDecrypt)
+        print(msg)
 
         close(con)    
     }
--- Chat/Servidor.r
+++ Chat/Servidor.r
@@ -1,63 +1,93 @@
-require (primes)
-require (FRACTION)
+require(primes)
+require(FRACTION)
+require(stringi)
 
 server <- function() {
+    #--------------------------------------------------------------------
     #Geração das Chaves Públicas e Privadas
+    maxPrimeNumber = 99
     print("Geracao da Chave Publica")
     isPrime = FALSE
     while(isPrime == FALSE) {
-        primNumber = sample(1:99,1)
+        primNumber = sample(1:maxPrimeNumber,1)
         if (primes::is_prime(primNumber)) {
             isPrime = TRUE
         }
     }
     
-    p = primNumber
+    serv_p = primNumber
 
     isPrime = FALSE
     while(isPrime == FALSE) {
-        primNumber = sample(1:99,1)
-        if (primNumber != p && primes::is_prime(primNumber)) {
+        primNumber = sample(1:maxPrimeNumber,1)
+        if (primNumber != serv_p && primes::is_prime(primNumber)) {
             isPrime = TRUE
         }
     }
 
-    q = primNumber
-    
-    n_Serv = p*q
+    serv_q = primNumber
 
-    fiN = (p-1)*(q-1)
+    serv_n = serv_p*serv_q
+
+    serv_n_fi = (serv_p-1)*(serv_q-1)
     
-    e_Serv = 0
-    for (i in 2:fiN) {
-        if (gcd(fiN, i) == 1) {
-            e_Serv = i
+    serv_e = 0
+    for (i in 2:serv_n_fi) {
+        if (gcd(serv_n_fi, i) == 1) {
+            serv_e = i
             break
         }
     }
 
-    rm(primNumber)
-    rm(isPrime)
-    rm(i)
+    serv_d = 1
+    repeat {
+        if (serv_d < serv_n_fi && (serv_d*serv_e) %% serv_n_fi == 1) {
+            break
+        }
+        serv_d = serv_d + 1
+    }
     #--------------------------------------------------------------------
-
     print("Troca de chaves")
-    #kpus = c(n_Serv,e_Serv)
-    kpus = c(toString(n_Serv), toString(e_Serv))
+    #kpus = c(serv_n,serv_e)
+    kpus = paste(c(serv_n, serv_e), collapse = ",")
     con <- socketConnection(host="localhost", port=666, blocking=TRUE, server=TRUE, open="r+")
 
     # receber a chave publica do cliente
-    kpuc = readLines(con,2)
+    kpuc = readLines(con,1)
     print(kpuc)
+
     # enviar a chave publica do servidor
-    write_resp = writeLines(kpus, con, useBytes = TRUE)
+    write_resp = writeLines(kpus, con)
+
+    cli_n = as.integer(unlist(strsplit(kpuc, split=",")))[1]
+    cli_e = as.integer(unlist(strsplit(kpuc, split=",")))[2]
 
     close(con)
+    #--------------------------------------------------------------------    
+    rm(maxPrimeNumber)
+    rm(primNumber)
+    rm(isPrime)
+    rm(i)
+    rm(kpuc)
+    rm(kpus)
+    #--------------------------------------------------------------------
 
     while(TRUE){
         writeLines("Listening...")
         con <- socketConnection(host="localhost", port = 666, blocking=TRUE, server=TRUE, open="r+")
 
+        # servidor recebe mensagem enviada pelo cliente
+        msgCrypt = readLines(con, 1)
+        msgCrypt = as.integer(unlist(strsplit(msgCrypt, split=",")))
+
+        # servidor decriptografa a mensagem e a mostra na tela
+        # fazer aqui a decriptografia
+        msgDecrypt = (msgCrypt ^ serv_d) %% serv_n
+        
+        msg = intToUtf8(msgDecrypt)
+        print(msg)
+        
+        # servidor captura mensagem da entrada padrao (teclado)
         f <- file("stdin")
         open(f)
         writeLines("msg", sep=": ")
@@ -65,6 +95,12 @@ server <- function() {
         if(tolower(msg)=="q"){
             break
         }
+        
+        # servidor criptografa a mensagem e a envia para o cliente
+        # fazer aqui a criptografia
+        msgUTF8 = utf8ToInt(msg)
+        msgCrypt = (msgUTF8 ^ cli_e) %% cli_n
+        write_resp <- writeLines(paste(as.character(msgCrypt), collapse = ","), con)
 
         close(con)
     }
